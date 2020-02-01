e523337d3c7b1f918f4c1cdfc99d8e59d3394151
Ivan Lavoryk <lavoryk@gmail.com>
2020-02-01 22:52:25 +0200

    Fixed loose condition

--- Assets/Game/Scripts/UI/LooseLevel.cs
+++ Assets/Game/Scripts/UI/LooseLevel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LooseLevel : MonoBehaviour
 {
@@ -20,6 +21,11 @@ public class LooseLevel : MonoBehaviour
         {
             color.a += Time.deltaTime * 0.2f;
             Image.color = color;
+        }
+        else
+        {
+            Debug.Log("Start Clicked");
+            SceneManager.LoadScene("Game/Scenes/UI/Start");
         }
     }
 }
